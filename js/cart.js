cb82f01f381271be7b748e9500276754caf48f27
Demjke <36662752+Demjke@users.noreply.github.com>
2021-02-18 12:44:51 +0300

    Add files via upload

---
+++ js/cart.js
@@ -0,0 +1,85 @@
+const cartWrapper = document.querySelector('.cart-wrapper');
+
+window.addEventListener('click', function (event) {
+  if (event.target.hasAttribute('data-cart')) {
+    const card = event.target.closest('.card');
+
+    const productInfo = {
+      id: card.dataset.id,
+      imgSrc: card.querySelector('.product-img').getAttribute('src'),
+      title: card.querySelector('.item-title').innerText,
+      itemsInBox: card.querySelector('[data-items-in-box]').innerText,
+      weight: card.querySelector('.price__weight').innerText,
+      price: card.querySelector('.price__currency').innerText,
+      counter: card.querySelector('[data-counter]').innerText,
+    };
+
+    const itemInCart = cartWrapper.querySelector(`[data-id="${productInfo.id}"]`);
+    if (itemInCart) {
+      const counterElement = itemInCart.querySelector('[data-counter]');
+      counterElement.innerText = parseInt(counterElement.innerText) + parseInt(productInfo.counter);
+    } else {
+      const cartItemHTML = `<div class="cart-item" data-id="${productInfo.id}">
+      <div class="cart-item__top">
+        <div class="cart-item__img">
+          <img src="${productInfo.imgSrc}" alt="">
+        </div>
+        <div class="cart-item__desc">
+          <div class="cart-item__title">${productInfo.title}</div>
+          <div class="cart-item__weight">${productInfo.itemsInBox} / ${productInfo.weight}</div>
+          <div class="cart-item__details">
+            <div class="items items--small counter-wrapper">
+              <div class="items__control" data-action="minus">-</div>
+              <div class="items__current" data-counter="">${productInfo.counter}</div>
+              <div class="items__control" data-action="plus">+</div>
+            </div>
+            <div class="price">
+              <div class="price__currency">${productInfo.price}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+
+      cartWrapper.insertAdjacentHTML('beforeend', cartItemHTML);
+    }
+
+    card.querySelector('[data-counter]').innerText = '1';
+
+    toggleCartStatus();
+
+  }
+});
+
+function toggleCartStatus() {
+  const cartEmpty = document.querySelector('[data-cart-empty]');
+  const cartTotal = document.querySelector('.cart-total');
+  const orderForm = document.querySelector('#order-form');
+
+  if (cartWrapper.querySelectorAll('.cart-item').length > 0) {
+    cartEmpty.classList.add('none');
+    cartTotal.classList.remove('none');
+    orderForm.classList.remove('none');
+  } else {
+    cartEmpty.classList.remove('none');
+    cartTotal.classList.add('none');
+    orderForm.classList.add('none');
+  }
+
+  let totalPrice = 0;
+  cartWrapper.querySelectorAll('.cart-item').forEach(function (item) {
+    const counter = item.querySelector('[data-counter]').innerText;
+    const priceOneItem = item.querySelector('.price__currency').innerText;
+    const price = parseInt(counter) * parseInt(priceOneItem);
+    totalPrice += price;
+  });
+  cartTotal.querySelector('.total-price').innerText = totalPrice;
+
+let delivery = document.querySelector('.free');
+
+  if(totalPrice < 1000) {
+    delivery.innerText = '300 ₽';
+  } else {
+    delivery.innerText = 'бесплатно';
+  }
+}
